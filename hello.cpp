d452f736fa1dfcebb62d4673fd3621405067108a
arjunatapadkar <arjunatapadkar94@gmail.com>
2022-12-28 15:37:29 +0530

    the hello.cpp file had some changes

--- hello.cpp
+++ hello.cpp
@@ -5,9 +5,11 @@ using namespace std;
 
 int main(){
 
-    cout << "hello world !";
+  
 
     cout << "we are learning git ie version control";
+
+    cout << "this is new code inserted";
 
     return 0;
 }
